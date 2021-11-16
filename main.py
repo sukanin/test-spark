a98d2419dfd9bb2a74cfc65cd804566bbeeb4d29
Sukanin Manmak <sukanin@netkasystem.com>
2021-11-16 16:58:52 +0700

    update script

--- main.py
+++ main.py
@@ -1,49 +1,71 @@
-import os, datetime
-from databricks import koalas as ks
-from pyspark.sql.dataframe import DataFrame
-from pyspark.sql.types import *
-from pyspark.sql.functions import explode, split, col, sum, lit
-from pyspark.sql import SparkSession
+import argparse  # Arguments parser
+import ujson as json  # Fast JSON parser
+from termcolor import cprint  # Colors in the console output
 
-def apply_transforms(df: DataFrame) -> DataFrame:
+from modules import kafkaIO  # IO operations with kafka topics
 
-    # split _c0 column as it is a string and we want the population data from it
-    split_col = split(df['_c0'], '\t')
 
-    # add population column, group by country, sum population
-    return df \
-            .withColumn("population", split_col.getItem(2).cast('float')) \
-            .groupBy("country") \
-            .agg(col("country"), sum("population")).select(col("country"), col("sum(population)") \
-            .alias("population"))
+def process_results(data_to_process, producer, output_topic):
+    """
+    Process analyzed data and modify it into desired output.
+    :param data_to_process: analyzed data
+    :param producer: Kafka producer
+    :param output_topic: Kafka topic through which output is send
+    """
+
+    # Here you can format your results output and send it to the kafka topic
+    # <-- INSERT YOUR CODE HERE
+
+    # Example of a transformation function that selects values of the dictionary and dumps them as a string
+    results_output = '\n'.join(map(json.dumps, data_to_process.values()))
+
+    # Send desired output to the output_topic
+    kafkaIO.send_data_to_kafka(results_output, producer, output_topic)
+
+
+def process_input(input_data):
+    """
+    Process raw data and do MapReduce operations.
+    :param input_data: input data in JSON format to process
+    :return: processed data
+    """
+    # Here you can process input stream with MapReduce operations
+    # <-- INSERT YOUR CODE HERE
+
+    # Example of the map function that transform all JSONs into the key-value pair with the JSON as value and static key
+    modified_input = input_data.map(lambda json_data: (1, json_data))
+
+    return modified_input
+
 
 if __name__ == "__main__":
+    # Define application arguments (automatically creates -h argument)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-iz", "--input_zookeeper", help="input zookeeper hostname:port", type=str, required=True)
+    parser.add_argument("-it", "--input_topic", help="input kafka topic", type=str, required=True)
+    parser.add_argument("-oz", "--output_zookeeper", help="output zookeeper hostname:port", type=str, required=True)
+    parser.add_argument("-ot", "--output_topic", help="output kafka topic", type=str, required=True)
+    parser.add_argument("-m", "--microbatch", help="microbatch duration", type=int, required=False, default=5)
 
-    # build spark session
-    spark = SparkSession.builder.appName("KoalasPostgresDemo").getOrCreate()
+    # You can add your own arguments here
+    # See more at:
+    # https://docs.python.org/2.7/library/argparse.html
 
-    # Enable hadoop s3a settings
-    spark._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
-    spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider", \
-    "com.amazonaws.auth.InstanceProfileCredentialsProvider,com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
-    spark._jsc.hadoopConfiguration().set("fs.AbstractFileSystem.s3a.impl", "org.apache.hadoop.fs.s3a.S3A")
+    # Parse arguments
+    args = parser.parse_args()
 
-    # read data from publc bucket into Spark DF
-    data_path = "s3a://dataforgood-fb-data/csv/" 
-    df = spark.read.csv(data_path)
+    # Initialize input stream and parse it into JSON
+    ssc, parsed_input_stream = kafkaIO\
+        .initialize_and_parse_input_stream(args.input_zookeeper, args.input_topic, args.microbatch)
 
-    # apply spark transformations
-    transformedDF = df.transform(apply_transforms)
+    # Process input in the desired way
+    processed_input = process_input(parsed_input_stream)
 
-    # build Koalas DF from Spark DF, get median, convert back to Spark DataFrame, add column with current date
-    kDF = ks.DataFrame(transformedDF)
-    medianDF = kDF.median().withColumn
-    finalDF = medianDF.to_spark().withColumn("etl_time", lit(datetime.datetime.now()))
+    # Initialize kafka producer
+    kafka_producer = kafkaIO.initialize_kafka_producer(args.output_zookeeper)
 
-    # SQL metadata
-    properties = {"user": os.environ['PG_USER'],"postgres": os.environ['PG_PASSWORD'],"driver": "org.postgresql.Driver"}
-    url = f"jdbc:postgresql://{os.environ['PG_HOST']}:{os.environ['PG_PORT']}/{os.environ['PG_DB_NAME']}"
+    # Process computed data and send them to the output
+    kafkaIO.process_data_and_send_result(processed_input, kafka_producer, args.output_topic, process_results)
 
-    # write to db
-    finalDF.write.jdbc(url=url, table=os.environ['TABLE_NAME'], mode="overwrite", properties=properties)
+    # Start Spark streaming context
+    kafkaIO.spark_start(ssc)
